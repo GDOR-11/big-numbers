1aed32eef9aedcc22ff8217d7f81b7ad411eaaa0
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2024-01-09 23:39:47 -0300

    added ability to save every so often + a bit of refactoring

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use std::fs::{self, File};
+use std::io::Write;
+use rug::Integer;
+
+pub fn get_closest_calculated_number(number: u64, directory: &str) -> (u64, Integer) {
+    let calculated_nums: Vec<u64> = match fs::read_dir(directory) {
+        Ok(paths) => paths,
+        Err(reason) => panic!("Could not read directory '{directory}'\n\nReason: {reason}")
+    }.filter_map(|path| {
+        let path = String::from(path.ok()?.path().to_str()?);
+        path[directory.len() + 1..path.len() - 4].parse::<u64>().ok()
+    }).collect();
+
+    let mut closest_calculated_num = None;
+    for calculated_num in calculated_nums {
+        if calculated_num < number && !closest_calculated_num.is_some_and(|num| calculated_num < num) {
+            closest_calculated_num = Some(calculated_num);
+        } else if calculated_num == number { std::process::exit(0); }
+    }
+
+    if closest_calculated_num.is_none() { return (0, Integer::from(1)); }
+    let closest_calculated_num = closest_calculated_num.unwrap();
+
+    match fs::read_to_string(&format!("{directory}/{closest_calculated_num}.txt")) {
+        Ok(string) => {
+            println!("Found file with {closest_calculated_num} factorial!");
+            println!("Reading file...");
+            let factorial = Integer::from_str_radix(&string, 10);
+            if let Ok(factorial) = factorial {
+                println!("File read successfully!");
+                (closest_calculated_num, factorial)
+            } else {
+                println!("File was invalid");
+                (0, Integer::from(1))
+            }
+        },
+        Err(_) => (0, Integer::from(1))
+    }
+}
+
+pub fn save_factorial(number: u64, factorial: &Integer, directory: &str) -> Result<(), std::io::Error> {
+    File::create(format!("{directory}/{number}.txt"))?.write_all(factorial.to_string().as_bytes())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{env, fs::File, ops::MulAssign, io::Write};
-
-use rug::Integer;
+use std::env;
+use std::ops::MulAssign;
+use factorial_calculator::*;
 use indicatif::ProgressBar;
 
 const DIRECTORY: &str = "factorials";
@@ -8,26 +8,26 @@ const DIRECTORY: &str = "factorials";
 fn main() {
     let args: Vec<String> = env::args().collect();
     let num = args.get(1).expect("1 argument is required to run the program").parse::<u64>().expect("First argument must be a number!");
+    let save_interval = args.get(2).map(|arg| arg.parse::<u64>().expect("Second argument must be a number, if present"));
 
-    let filename = &format!("{DIRECTORY}/{num}.txt");
 
-    let mut file = match File::create(filename) {
-        Ok(file) => file,
-        Err(reason) => panic!("Could not create the file '{filename}'\n\nReason: {reason}")
-    };
+    let closest_calculated_number = get_closest_calculated_number(num, DIRECTORY);
+    println!("Calculating {num}!, starting from {}!", closest_calculated_number.0);
 
     let progress_bar = ProgressBar::new(num);
+    progress_bar.set_position(closest_calculated_number.0);
 
-    let mut factorial = Integer::from(1);
-    for x in 2..=num {
+    let mut factorial = closest_calculated_number.1;
+    for x in closest_calculated_number.0 + 1..=num {
         factorial.mul_assign(x);
         if x % 10000 == 0 {
             progress_bar.set_position(x);
         }
+        if save_interval.is_some_and(|save_interval| x % save_interval == closest_calculated_number.0 % save_interval) {
+            save_factorial(x, &factorial, DIRECTORY).expect(&format!("Could not save file '{DIRECTORY}/{x}.txt'"));            
+        }
     }
 
-    match file.write_all(factorial.to_string().as_bytes()) {
-        Ok(_) => (),
-        Err(reason) => panic!("Could not write to file '{filename}'\n\nReason: {reason}")
-    };
+    save_factorial(num, &factorial, DIRECTORY).expect(&format!("Could not save file '{DIRECTORY}/{num}.txt'"));
 }
+
