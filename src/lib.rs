943714c1d979eda74daf024f6b9980ce66eadbb8
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2024-01-16 12:22:57 -0300

    too lazy to write proper message

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,111 @@
+use std::fmt;
 use std::fs::{self, File};
 use std::io::Write;
 use rug::Integer;
+use termimad::MadSkin;
+use reqwest;
+
+pub struct CLIArguments {
+    pub target_number: u64,
+    pub save_step: Option<u64>,
+    pub use_remote_files: bool
+}
+pub enum CLIArgumentsError {
+    InvalidSyntax(String),
+    IncorrectArguments(String)
+}
+impl CLIArgumentsError {
+    fn invalid_syntax_default() -> Self {
+        Self::InvalidSyntax(String::from("Arguments must come in the form '<name>=<value>'"))
+    }
+}
+impl fmt::Display for CLIArgumentsError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidSyntax(string) => write!(f, "Syntax error: {}", string),
+            Self::IncorrectArguments(string) => write!(f, "Arguments error: {}", string)
+        }
+    }
+}
+pub fn interpret_arguments(args: Vec<String>) -> Result<CLIArguments, CLIArgumentsError> {
+    if args.get(1).is_some_and(|arg| arg == "--help" || arg == "-h") {
+        let text = fs::read_to_string("./help.md").unwrap_or("Help file not found, good luck figuring out how this works lmao".to_string());
+        let skin = MadSkin::default();
+        println!("{}", skin.term_text(&text));
+        std::process::exit(0);
+    }
+
+    let (mut target_number, mut save_step, mut use_remote_files) = (None, None, None);
+    for arg in args.split_at(1).1 {
+        let Some((name, value)) = arg.split_once('=') else {
+            return Err(CLIArgumentsError::invalid_syntax_default());
+        };
+        match name {
+            "target" => {
+                target_number = value.parse::<u64>().ok();
+                if target_number == None {
+                    return Err(CLIArgumentsError::IncorrectArguments(format!("target must be a non-negative integer less than or equal to {}", u64::MAX)))
+                }
+            },
+            "save-step" => {
+                save_step = value.parse::<u64>().ok();
+                if save_step == None || save_step == Some(0) {
+                    return Err(CLIArgumentsError::IncorrectArguments(format!("save-step must be a non-negative integer less than or equal to {}", u64::MAX)))
+                }
+            },
+            "use-git-sparse-checkout" => {
+                use_remote_files = match value {
+                    "yes" | "true" | "easter egg :D" => Some(true),
+                    "no" | "false" => Some(false),
+                    _ => return Err(CLIArgumentsError::IncorrectArguments(format!("use-remote-files must be 'yes'/'true' or 'no'/'false'")))
+                }
+            },
+            _ => return Err(CLIArgumentsError::IncorrectArguments(format!("Argument '{name}' does not exist")))
+        }
+    }
+    let Some(target_number) = target_number else {
+        return Err(CLIArgumentsError::IncorrectArguments(format!("target number is missing")));
+    };
+    let use_remote_files = use_remote_files.unwrap_or(false);
+
+    Ok(CLIArguments { target_number, save_step, use_remote_files })
+}
 
 pub fn filepath(directory: &str, number: u64) -> String {
     format!("{directory}/{number}/{number}.txt")
 }
 
-pub fn get_closest_calculated_number(number: u64, directory: &str) -> (u64, Integer) {
-    let calculated_nums: Vec<u64> = match fs::read_dir(directory) {
-        Ok(paths) => paths,
-        Err(_) => return (0, Integer::from(1))
-    }.filter_map(|path| {
-        let path = String::from(path.ok()?.path().to_str()?);
-        path[directory.len() + 1..].parse::<u64>().ok()
-    }).collect();
+async fn read_file(filepath: &str, from_remote: bool) -> Option<String> {
+    if from_remote {
+        reqwest::get(
+            format!("https://raw.githubusercontent.com/GDOR-11/factorial-calculator/main/{filepath}")
+        ).await.ok()?.text().await.ok()
+    } else {
+        fs::read_to_string(filepath).ok()
+    }
+}
+
+pub async fn get_closest_calculated_number(number: u64, directory: &str, use_remote_files: bool) -> Option<(u64, Integer)> {
+    let calculated_nums: Vec<u64> =
+        read_file(&format!("{directory}/factorials/factorials.txt"), use_remote_files).await?
+        .split('\n')
+        .filter_map(|str| str.parse::<u64>().ok())
+        .collect();
 
     let mut closest_calculated_num = None;
     for calculated_num in calculated_nums {
-        if calculated_num < number && !closest_calculated_num.is_some_and(|num| calculated_num < num) {
+        if calculated_num <= number && !closest_calculated_num.is_some_and(|num| calculated_num < num) {
             closest_calculated_num = Some(calculated_num);
-        } else if calculated_num == number {
-            println!("File already exists!");
-            std::process::exit(0);
         }
     }
+    let Some(closest_calculated_num) = closest_calculated_num else { return None; };
 
-    if closest_calculated_num.is_none() { return (0, Integer::from(1)); }
-    let closest_calculated_num = closest_calculated_num.unwrap();
+    let factorial = Integer::from_str_radix(
+        &read_file(&filepath(directory, closest_calculated_num), use_remote_files).await?,
+        10
+    ).ok()?;
 
-    match fs::read_to_string(filepath(directory, closest_calculated_num)) {
-        Ok(file) => {
-            println!("Found file with {closest_calculated_num} factorial!");
-            println!("Processing file...");
-            if let Ok(factorial) = Integer::from_str_radix(&file, 10) {
-                println!("File read successfully!");
-                (closest_calculated_num, factorial)
-            } else {
-                println!("File was invalid");
-                (0, Integer::from(1))
-            }
-        },
-        Err(_) => (0, Integer::from(1))
-    }
+    Some((closest_calculated_num, factorial))
 }
 
 pub fn save_factorial(number: u64, factorial: &Integer, directory: &str) -> Result<(), std::io::Error> {
--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,40 @@ use std::env;
 use std::ops::MulAssign;
 use factorial_calculator::*;
 use indicatif::ProgressBar;
+use rug::Integer;
+
 
 const DIRECTORY: &str = "factorials";
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let number = args.get(1).expect("1 argument is required to run the program").parse::<u64>().expect("First argument must be a number!");
-    let save_interval = args.get(2).map(|arg| arg.parse::<u64>().expect("Second argument must be a number, if present"));
+#[tokio::main]
+async fn main() {
+    let arguments = interpret_arguments(env::args().collect()).unwrap_or_else(|error| {
+        eprintln!("{error}");
+        println!("Hint: use --help if you don't know how this tool works");
+        std::process::exit(0);
+    });
+
+    let target = arguments.target_number;
+    let save_step = arguments.save_step;
+    let use_remote_files = arguments.use_remote_files;
 
 
-    let closest_calculated_number = get_closest_calculated_number(number, DIRECTORY);
-    println!("Calculating {number}!, starting from {}!", closest_calculated_number.0);
+    let closest_calculated_number = get_closest_calculated_number(target, DIRECTORY, use_remote_files).await.unwrap_or((0, Integer::from(1)));
+    println!("Calculating {target}!, starting from {}!", closest_calculated_number.0);
 
-    let progress_bar = ProgressBar::new(number);
+    let progress_bar = ProgressBar::new(target);
     progress_bar.set_position(closest_calculated_number.0);
 
     let mut factorial = closest_calculated_number.1;
-    for x in closest_calculated_number.0 + 1..=number {
+    for x in closest_calculated_number.0 + 1..=target {
         factorial.mul_assign(x);
         if x % 10000 == 0 {
             progress_bar.set_position(x);
         }
-        if save_interval.is_some_and(|save_interval| x % save_interval == closest_calculated_number.0 % save_interval) {
+        if save_step.is_some_and(|save_step| x % save_step == 0) {
             save_factorial(x, &factorial, DIRECTORY).expect(&format!("Could not save file '{}'", filepath(DIRECTORY, x)));            
         }
     }
 
-    save_factorial(number, &factorial, DIRECTORY).expect(&format!("Could not save file '{}'", filepath(DIRECTORY, number)));
+    save_factorial(target, &factorial, DIRECTORY).expect(&format!("Could not save file '{}'", filepath(DIRECTORY, target)));
 }
-
