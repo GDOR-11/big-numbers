5ce70b2e44c3887b58c02b02d9e7ba9658189576
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2024-01-26 22:01:09 -0300

    fix that I forgot to push

--- src/remote_files_handler.rs
+++ src/remote_files_handler.rs
@@ -1,7 +1,7 @@
 use crate::local_files_handler;
 use reqwest;
 use std::path::Path;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::time::SystemTime;
 
 #[derive(Debug)]
@@ -13,9 +13,11 @@ pub enum RemoteError {
 }
 
 pub async fn read_file(file_path: &str) -> Result<String, RemoteError> {
+    // use the current time to generate a new token with every request
     let now = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap();
+
     reqwest::get(
         format!("https://raw.githubusercontent.com/GDOR-11/factorial-calculator/main/{file_path}?token={:?}", now)
     ).await
@@ -41,32 +43,40 @@ pub fn write_file(file_path: &str, file_content: &str) -> Result<(), RemoteError
     // git add --sparse <file path>
     // git commit -m "Adding files automatically"
     // git push origin main
+    // rm -rf <parent directory>
+    // git sparse-checkout reapply
     Command::new("git")
         .arg("reset")
-        .stdout(std::process::Stdio::null())
+        .stdout(Stdio::null())
         .status()
         .map_err(|error| RemoteError::GitExecutionError(error))?;
     Command::new("git")
         .args(["add", "--sparse", file_path])
-        .stdout(std::process::Stdio::null())
         .status()
         .map_err(|error| RemoteError::GitExecutionError(error))?;
     Command::new("git")
         .args(["commit", "-m", "Adding files automatically"])
-        .stdout(std::process::Stdio::null())
+        .stdout(Stdio::null())
         .status()
         .map_err(|error| RemoteError::GitExecutionError(error))?;
     Command::new("git")
         .args(["push", "origin", "main"])
-        .stderr(std::process::Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
         .status()
         .map_err(|error| RemoteError::GitExecutionError(error))?;
+
     local_files_handler::delete_path(
         Path::new(file_path)
             .parent()
             .expect("dont mess with the code"),
     )
     .map_err(|error| RemoteError::LocalError(error))?;
+
+    Command::new("git")
+        .args(["sparse-checkout", "reapply"])
+        .status()
+        .map_err(|error| RemoteError::GitExecutionError(error))?;
 
     Ok(())
 }
