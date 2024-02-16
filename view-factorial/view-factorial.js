be026d37b9b1386bbdbb51cb4fa3cc7c2b0306e5
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2024-02-16 17:09:14 -0300

    frontend refactor

--- view-factorial/view-factorial.js
+++ view-factorial/view-factorial.js
@@ -1,16 +1,15 @@
-const query_string = window.location.search;
-const query = query_string.slice(1).split("&").map(string => string.split("="));
-const number = parseInt(query.find(arg => arg[0] == "factorial")?.[1]);
+const query = window.location.search.slice(1).split("&").map(string => string.split("="));
+const number = query.find(arg => arg[0] == "number")?.[1];
 const base = parseInt(query.find(arg => arg[0] == "base")?.[1]) || 10;
 
-if(isNaN(number)) {
-    alert("please provide a valid number to view the factorial of");
+if(!number) {
+    alert("please provide a valid number to view");
     throw "hello there, why are you peeking in the console";
 }
 
-document.title = `${number}!` + (base == 10 ? `` : ` base ${base}`);
+document.title = `${number}` + (base == 10 ? `` : ` base ${base}`);
 
-const text_p = document.getElementById("factorial");
+const text_p = document.getElementById("big-number");
 async function update_text(text) {
     text_p.innerText = text;
     await new Promise(r => setTimeout(r, 1000));
@@ -51,50 +50,67 @@ function estimate_bigint_toString_time(bit_length, base) {
     return time * (bit_length * Math.log(bit_length)) / (small_bit_length * Math.log(small_bit_length));
 }
 
-async function arraybuffer_to_string(array_buffer, base) {
-    const data_view = new DataView(array_buffer);
+async function binary_number_to_string(number, base) {
+    const response = await fetch(`../binary-bigints/${number}/${number}.bigint`);
+    const blob = await response.blob(); response = null;
+    const array_buffer = await blob.arrayBuffer(); blob = null;
+    const data_view = new DataView(array_buffer); array_buffer = null;
 
     await update_text("converting buffer to string...");
-    let str = "0x";
+    let string = "0x";
 
     let offset = 0;
     for(;offset < data_view.byteLength - 3;offset += 4) {
-        str += u32_to_hex(data_view.getUint32(offset));
+        string += u32_to_hex(data_view.getUint32(offset));
     }
     for(;offset < data_view.byteLength;offset++) {
-        str += u8_to_hex(data_view.getUint8(offset));
+        string += u8_to_hex(data_view.getUint8(offset));
     }
 
+    data_view = null;
+
     if(base == 16) {
-        return str.slice(2);
+        return string.slice(2);
     } else {
         await update_text("parsing string into BigInt...");
-        let bigint = BigInt(str);
+        let bigint = BigInt(string);
+        const bit_length = (string.length - 2) * 4; string = null;
 
         await update_text("measuring device performance...");
-        let estimated_time = estimate_bigint_toString_time((str.length - 2) * 4, base);
+        let estimated_time = estimate_bigint_toString_time(bit_length, base);
 
         await update_text(`converting bigint into base ${base} string...\nestimated time: ${Math.round(estimated_time) / 1000}s`);
         return bigint.toString(base);
     }
 }
 
-async function get_factorial(number, base) {
+async function decimal_number_to_string(number, base) {
+    const response = await fetch(`../decimal-bigints/${number}/${number}.txt`);
+    const text = response.text(); response = null;
+
+    // it makes no sense to do nothing
+    // this single line makes 90% of use cases a LOT more efficient
+    if(base == 10) return text;
+
+    await update_text(`converting from base ${base} string to bigint...`);
+    let bigint = BigInt(text); text = null;
+
+    await update_text(`converting from bigint to base ${base} string`);
+    return bigint.toString(base);
+}
+
+async function get_number(number, base) {
     await update_text("fetching data...");
     try {
-        const response = await fetch(`../binary-bigints/${number}/${number}.bigint`);
-        const blob = await response.blob();
-        const array_buffer = await blob.arrayBuffer();
-        return arraybuffer_to_string(array_buffer, base);
+        return await binary_number_to_string(number, base);
     } catch(err) {
-        const response = await fetch(`../decimal-bigints/${number}/${number}.txt`);
-        return await response.text();
+        return await decimal_number_to_string(number, base);
     } finally {
         await update_text("number not found, check if you haven't made a typo");
     }
 }
 
-const factorial = await get_factorial(number, base);
+const big_number = await get_number(number, base);
 
 await update_text("displaying...");
-text_p.innerText = factorial;
+text_p.innerText = big_number;
