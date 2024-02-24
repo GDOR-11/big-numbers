b514ac7cbf0127614f956b6f19bb668edc226c17
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2024-02-24 13:28:19 -0300

    oops

--- src/factorial/factorial_calculator.rs
+++ src/factorial/factorial_calculator.rs
@@ -4,14 +4,14 @@ use primal::Sieve;
 // translation and optimization of https://www.luschny.de/math/factorial/csharp/FactorialPrimeSwing.cs.html
 
 pub fn factorial(n: usize) -> Integer {
-    let mut primes = Sieve::new(n).primes_from(3).collect();
+    let primes = &Sieve::new(n).primes_from(3).collect();
 
     let exp_2 = n - n.count_ones() as usize;
     rec_factorial(primes, n) << exp_2
 }
 
 
-fn rec_factorial(primes: Vec<usize>, n: usize) -> Integer {
+fn rec_factorial(primes: &Vec<usize>, n: usize) -> Integer {
     if n < 2 {
         Integer::from(1)
     } else {
@@ -23,7 +23,7 @@ const SMALL_ODD_SWING: &[usize] = &[1, 1, 1, 3, 3, 15, 5, 35, 35, 315, 63, 693, 
                                     12155, 230945, 46189, 969969, 88179, 2028117, 676039, 16900975, 1300075, 35102025,
                                     5014575, 145422675, 9694845, 300540195, 300540195];
 
-fn swing(primes: Vec<usize>, n: usize) -> Integer {
+fn swing(primes: &Vec<usize>, n: usize) -> Integer {
     if n < SMALL_ODD_SWING.len() {
         return Integer::from(SMALL_ODD_SWING[n]);
     }
@@ -33,7 +33,7 @@ fn swing(primes: Vec<usize>, n: usize) -> Integer {
     let root_n = (n as f64).sqrt().floor() as usize;
     let n_over_3 = n / 3;
 
-    for &prime in &primes {
+    for &prime in primes {
         if prime > root_n {
             if prime > n_over_3 { break; }
             if (n / prime) % 2 == 1 {
@@ -57,7 +57,7 @@ fn swing(primes: Vec<usize>, n: usize) -> Integer {
 }
 
 // doing this is faster than directly calling .iter().product()
-fn primorial(primes: Vec<usize>, start: usize, end: usize) -> Integer {
+fn primorial(primes: &Vec<usize>, start: usize, end: usize) -> Integer {
     if end - start < 2048 {
         // if the length is small enough, multiply everything directly (more cache efficiency,
         // less recursion overhead, etc.)
